ca9618236c2637b5f56a267edbf4e280e6e0d7ab
ValeriaRodriguezJ <rodriguz95@gmail.com>
2022-10-16 17:03:36 -0500

    Add styles of header

---
+++ src/components/header/styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from '@mui/styles'
+const useStyles = makeStyles(() => ({
+  wrapper: {
+    background: '#549BB3',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  title: {
+    color: 'white',
+    marginTop: '0.5rem'
+  }
+}))
+
+export default useStyles
