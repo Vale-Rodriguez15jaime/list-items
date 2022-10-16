dd28dd020858b9fba96e5ca9fd36a64c22b8aa98
ValeriaRodriguezJ <rodriguz95@gmail.com>
2022-10-16 18:00:10 -0500

    Add theme and configure this

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.sass'
+import { ThemeProvider } from '@mui/material'
+import theme from 'theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
