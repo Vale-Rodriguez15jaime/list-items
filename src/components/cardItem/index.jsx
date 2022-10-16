0afcd6a509e837979fe7ba12238a41569d406fd0
ValeriaRodriguezJ <rodriguz95@gmail.com>
2022-10-16 18:53:12 -0500

    Add component card

---
+++ src/components/cardItem/index.jsx
@@ -0,0 +1,35 @@
+import {
+  Button,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  Card
+} from '@mui/material'
+
+const CardItem = () => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Portrait_of_an_Iguana.jpg/1280px-Portrait_of_an_Iguana.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000 species,
+          ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default CardItem
