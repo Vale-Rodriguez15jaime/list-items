87f6e9f29c26ad7239729acc37dc36a554ce3a6b
ValeriaRodriguezJ <rodriguz95@gmail.com>
2022-10-16 17:02:54 -0500

    Update header and add component layout

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,20 @@
-const Header = () => {
-  return <div>hl</div>
+import Logo from 'assets/logo'
+import useStyles from './styles'
+
+const Header = ({ currentPosition }) => {
+  const classes = useStyles()
+  return (
+    <div>
+      <div className={classes.wrapper}>
+        <Logo />
+        <div className={classes.title}>{currentPosition}</div>
+      </div>
+    </div>
+  )
+}
+
+Header.defaultProps = {
+  currentPosition: 'List'
 }
 
 export default Header
