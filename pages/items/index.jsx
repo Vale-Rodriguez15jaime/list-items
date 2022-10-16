a1a2cffb562959e45a3bf4a755b4bc3f47e748d1
ValeriaRodriguezJ <rodriguz95@gmail.com>
2022-10-16 18:52:13 -0500

    Add page items

---
+++ pages/items/index.jsx
@@ -0,0 +1,12 @@
+import Layout from 'components/layout'
+import ListItems from 'modules/listItems'
+
+const List = () => {
+  return (
+    <Layout page="List">
+      <ListItems />
+    </Layout>
+  )
+}
+
+export default List
