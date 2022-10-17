7ca9e93462a0e84a19d50558f6837e60650ab4f3
ValeriaRodriguezJ <rodriguz95@gmail.com>
2022-10-17 09:39:50 -0500

    Add get list when search exist

--- src/modules/listItems/index.jsx
+++ src/modules/listItems/index.jsx
@@ -1,33 +1,54 @@
+import { useReducer, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { Grid } from '@mui/material'
+import { Grid, CircularProgress, Box, Typography } from '@mui/material'
 
 import CardItem from 'components/cardItem'
 import useStyles from './styles'
+import { initialState, reducer } from './reducer'
+import { getList } from './actions'
 
 const ListItems = () => {
   const classes = useStyles()
   const router = useRouter()
-  console.log('router', router.query)
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const search = router.query.search || ''
+  const { isLoading, data } = state
+
+  useEffect(() => {
+    if (search !== '') {
+      getList({ search, page_size: 4 }, dispatch).then()
+    }
+  }, [search])
 
   const handleClickLearnMore = id => {
     router.push('/items/[id]', `/items/${id}`)
   }
 
   return (
-    <Grid
-      className={classes.listContainer}
-      container
-      rowSpacing={2}
-      columnSpacing={{ xs: 1, sm: 4, md: 3 }}
-    >
-      {[...Array(20).keys()].map(item => {
-        return (
-          <Grid key={item} item>
-            <CardItem onClick={() => handleClickLearnMore(item)} />
-          </Grid>
-        )
-      })}
-    </Grid>
+    <>
+      {isLoading && (
+        <Box sx={{ display: 'flex' }}>
+          <CircularProgress />
+        </Box>
+      )}
+      {!isLoading && data.length > 0 && (
+        <Grid
+          className={classes.listContainer}
+          container
+          rowSpacing={2}
+          columnSpacing={{ xs: 1, sm: 4, md: 3 }}
+        >
+          {data.map(item => {
+            return (
+              <Grid key={item.id} item>
+                <CardItem onClick={() => handleClickLearnMore(item.id)} />
+              </Grid>
+            )
+          })}
+        </Grid>
+      )}
+      {!isLoading && data.length === 0 && <Typography>Empty list</Typography>}
+    </>
   )
 }
 
