8387d9f48bb074a3ea35947d37922d3f4b9e3486
ValeriaRodriguezJ <rodriguz95@gmail.com>
2022-10-17 09:40:25 -0500

    Add get Post in page detail

--- pages/items/[id]/index.jsx
+++ pages/items/[id]/index.jsx
@@ -1,12 +1,28 @@
 import Layout from 'components/layout'
 import DetailItem from 'modules/detailItem'
+import { getPost } from 'modules/listItems/actions'
 
-const Detail = () => {
+const Detail = ({ data }) => {
   return (
     <Layout page="Detail">
-      <DetailItem />
+      <DetailItem data={data} />
     </Layout>
   )
+}
+
+export async function getServerSideProps(context) {
+  try {
+    const { params } = context
+    const id = params.id
+    const data = await getPost(id)
+    return {
+      props: { data: data.data }
+    }
+  } catch (e) {
+    return {
+      notFound: true
+    }
+  }
 }
 
 export default Detail
--- src/modules/detailItem/index.jsx
+++ src/modules/detailItem/index.jsx
@@ -1,40 +1,23 @@
 import { Box, Typography, Container } from '@mui/material'
 import useStyles from './styles'
 
-const DetailItem = () => {
+const DetailItem = ({ data = null }) => {
   const classes = useStyles()
   return (
     <Container>
-      <Box className={classes.title}>
-        <Typography variant="h2">Este es un titulo</Typography>
-      </Box>
-      <Box className={classes.imageContainer}>
-        <img
-          className={classes.image}
-          alt="detail"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Portrait_of_an_Iguana.jpg/1280px-Portrait_of_an_Iguana.jpg"
-        />
-      </Box>
-      <Typography variant="body1" gutterBottom>
-        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
-        tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus,
-        cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-        unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate
-        numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.Lorem ipsum
-        dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit,
-        quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-        dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.Lorem ipsum dolor
-        sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam
-        beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos
-        laborum fugiat deleniti? Eum quasi quidem quibusdam.Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
-        rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-        fugiat deleniti? Eum quasi quidem quibusdam.Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore
-        consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat
-        deleniti? Eum quasi quidem quibusdam.
-      </Typography>
+      {data && (
+        <>
+          <Box className={classes.title}>
+            <Typography variant="h2">{data.title}</Typography>
+          </Box>
+          <Box className={classes.imageContainer}>
+            <img className={classes.image} alt="detail" src={data.image_url} />
+          </Box>
+          <Typography variant="body1" gutterBottom>
+            {data.description}
+          </Typography>
+        </>
+      )}
     </Container>
   )
 }
