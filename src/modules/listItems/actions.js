3dc74c5fead535536fdef6d2d981e8ce965a926f
ValeriaRodriguezJ <rodriguz95@gmail.com>
2022-10-18 20:03:05 -0500

    Add page size

--- src/modules/listItems/actions.js
+++ src/modules/listItems/actions.js
@@ -6,7 +6,7 @@ export const baseUrl = 'http://ec2-54-146-207-213.compute-1.amazonaws.com/api/v1
 export const getList = async (params, dispatch) => {
   try {
     dispatch({ type: actionType.GET_LIST })
-    const url = `${baseUrl}?search=${params.search}`
+    const url = `${baseUrl}?search=${params.search}&page_size=${params.pageSize || 4}`
     const res = await axios.get(url)
     dispatch({ type: actionType.GET_LIST_SUCCESS, payload: res })
   } catch (error) {
