af7d80e26c605da7987c29a4447487f16d63b5f7
ValeriaRodriguezJ <rodriguz95@gmail.com>
2022-10-18 20:03:49 -0500

    Update card

--- src/components/cardItem/index.jsx
+++ src/components/cardItem/index.jsx
@@ -7,22 +7,13 @@ import {
   Card
 } from '@mui/material'
 
-const CardItem = ({ onClick }) => {
+const CardItem = ({ onClick, item }) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Portrait_of_an_Iguana.jpg/1280px-Portrait_of_an_Iguana.jpg"
-        alt="green iguana"
-      />
+      <CardMedia component="img" height="140" image={item.image_url} alt="green iguana" />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000 species,
-          ranging across all continents except Antarctica
+          {item.title}
         </Typography>
       </CardContent>
       <CardActions>
